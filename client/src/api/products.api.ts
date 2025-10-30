a87604fc8274f7912300055043ac1123960d8f9e
FullDevJuan <juandev003@gmail.com>
2025-10-30 18:22:01 -0500

    corrección de ruta a petición de productos

--- client/src/api/products.api.ts
+++ client/src/api/products.api.ts
@@ -2,7 +2,7 @@ const PRODUCTS_SERVICE_URL = import.meta.env.VITE_PRODUCTS_SERVICE_URL;
 
 export async function getProducts() {
   try {
-    const req = await fetch(`${PRODUCTS_SERVICE_URL}/products`);
+    const req = await fetch(`${PRODUCTS_SERVICE_URL}/api/products`);
     const data = await req.json();
     console.log(data);
   } catch (error) {
