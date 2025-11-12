378ef51980a3f734823f8082c0024c6967b20ea6
FullDevJuan <juandev003@gmail.com>
2025-11-11 21:47:57 -0500

    controlador de products complementado

--- services/products-service/src/controllers/productControllers.js
+++ services/products-service/src/controllers/productControllers.js
@@ -1,3 +1,4 @@
+import { text } from "express";
 import { pool } from "../../../../shared/config/db.js";
 
 export async function read(req, res) {
@@ -6,5 +7,103 @@ export async function read(req, res) {
     res.status(200).json(rows);
   } catch (error) {
     res.status(500).json({ error: "Error interno del servidor" });
+  }
+}
+
+// TODO: ver todos los clientes que tiene un producto
+export async function getCustomersByProductId(req, res) {
+  const { id } = req.params;
+  const query = `SELECT 
+    c.id AS customer_id,
+    c.name AS customer,
+    fp.name AS product,
+    fp.product_type,
+    fp.interest_rate,
+    cp.current_balance,
+    cp.profitability,
+    cp.details AS customer_product_details,
+    fp.details AS product_details,
+    cp.acquisition_date,
+    cp.status
+    FROM customer_products cp
+    INNER JOIN customers c ON cp.customer_id = c.id
+    INNER JOIN financial_products fp ON cp.product_id = fp.id
+    WHERE fp.id='${id}'`;
+  try {
+    const { rows } = await pool.query(query);
+    if (rows.length === 0) {
+      return res.status(404).json({ error: "No customers for this product" });
+    }
+    res.status(200).json(rows);
+  } catch (error) {
+    console.error("Error executing query", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
+
+// TODO: crear producto
+export async function createProduct(req, res) {
+  const { body } = req;
+
+  try {
+    const query = {
+      text: "INSERT INTO financial_products (name, product_type, description, interest_rate, status, details) VALUES ($1, $2, $3, $4, $5, $6)",
+      values: [
+        body.name,
+        body.product_type,
+        body.description,
+        body.interest_rate,
+        body.status,
+        body.details,
+      ],
+    };
+
+    const { rows } = await pool.query(query);
+    res.status(201).json({
+      msg: "Product successfully created",
+      user: rows[0],
+    });
+  } catch (error) {
+    console.error("Error creating product:", error);
+    res.status(500).json({
+      msg: "Error creating product",
+      error: error.message,
+    });
+  }
+}
+
+// TODO: actualizar producto
+export async function updateProduct(req, res) {
+  const { body } = req;
+
+  try {
+    const query = {
+      text: `UPDATE financial_products SET
+      name = $1,
+      product_type = $2,
+      description = $3,
+      interest_rate = $4,
+      status = $5,
+      details = $6
+      WHERE id = $7`,
+      values: [
+        body.name,
+        body.product_type,
+        body.description,
+        body.interest_rate,
+        body.status,
+        body.details,
+        body.id,
+      ],
+    };
+
+    const { rows } = await pool.query(query);
+    res.status(200).json({ msg: "Product successfully updated" });
+  } catch (error) {
+    console.error("Error updating product:", error);
+    res.status(500).json({
+      msg: "Error updating product",
+      error: error.message,
+    });
   }
 }
--- services/products-service/src/routes/productRoutes.js
+++ services/products-service/src/routes/productRoutes.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
-import { read } from "../controllers/productControllers.js";
+import {
+  createProduct,
+  getCustomersByProductId,
+  read,
+  updateProduct,
+} from "../controllers/productControllers.js";
 import { verifyToken } from "../../../../shared/middlewares/auth.js";
 
 const router = Router();
 
 router.get("/", verifyToken, read);
+router.get("/search/customers/:id", verifyToken, getCustomersByProductId);
+router.post("/create", verifyToken, createProduct);
+router.put("/update", verifyToken, updateProduct);
 
 export default router;
