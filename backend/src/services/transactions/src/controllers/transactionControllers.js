0571550cffb2020499b7988081d25cdd13a0218d
FullDevJuan <juandev003@gmail.com>
2025-11-12 10:12:26 -0500

    controlador de transactions complementado

--- backend/src/services/transactions/src/controllers/transactionControllers.js
+++ backend/src/services/transactions/src/controllers/transactionControllers.js
@@ -2,10 +2,43 @@ import { pool } from "../../../../../../shared/config/db.js";
 
 export async function read(req, res) {
   try {
-    const { rows } = await pool.query("SELECT * FROM transactions");
-    res.json(rows);
+    const { rows } = await pool.query(`SELECT 
+    a.account_number,
+    a.account_type,
+    t.amount,
+    t.transaction_type,
+    t.created_at AS date,
+    (SELECT a.account_number FROM accounts a WHERE a.id = (t.details->>'destination_account_id')::uuid) AS destination_account_number
+    FROM accounts a
+    INNER JOIN transactions t ON a.id = t.account_id`);
+
+    res.status(200).json(rows);
   } catch (error) {
     console.log(error);
     res.status(500).send("Error fetching transactions");
+  }
+}
+
+// TODO: registrar transaccion
+export async function createTransaction(req, res) {
+  const { body } = req;
+  try {
+    const query = {
+      text: `INSERT INTO transactions (account_id, transaction_type, amount, details) VALUES ($1, $2, $3, $4)`,
+      values: [
+        body.account_id,
+        body.transaction_type,
+        body.amount,
+        body.details,
+      ],
+    };
+    const { rows } = await pool.query(query);
+    res.status(200).json({ msg: "Transaction successfully created" });
+  } catch (error) {
+    console.error("Error creating transaction:", error);
+    res.status(500).json({
+      msg: "Error creating transaction",
+      error: error.message,
+    });
   }
 }
--- backend/src/services/transactions/src/routes/transactionRoutes.js
+++ backend/src/services/transactions/src/routes/transactionRoutes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { read } from "../controllers/transactionControllers.js";
+import {
+  createTransaction,
+  read,
+} from "../controllers/transactionControllers.js";
 import { verifyToken } from "../../../../../../shared/middlewares/auth.js";
 
 const router = Router();
 router.get("/", verifyToken, read);
+router.post("/create", verifyToken, createTransaction);
 
 export default router;
