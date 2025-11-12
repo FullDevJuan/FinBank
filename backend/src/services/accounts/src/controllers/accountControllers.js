544f7fd097f2dd68a6927fbd9e4339fab7b1c5e3
FullDevJuan <juandev003@gmail.com>
2025-11-11 21:46:34 -0500

    controlador de accounts complementado

--- backend/src/services/accounts/src/controllers/accountControllers.js
+++ backend/src/services/accounts/src/controllers/accountControllers.js
@@ -1,12 +1,48 @@
 import { pool } from "../../../../../../shared/config/db.js";
 
 export async function read(req, res) {
-  const query = `SELECT c.name AS customer, cp.current_balance, cp.status, fp.name AS product, a.account_type, a.account_number FROM accounts a INNER JOIN customer_products cp ON a.customer_product_id = cp.id INNER JOIN customers c ON cp.customer_id = c.id INNER JOIN financial_products fp ON cp.product_id = fp.id`;
+  const query = `SELECT 
+  a.id,
+  c.name AS customer, 
+  cp.current_balance, 
+  cp.status, 
+  fp.name AS product, 
+  a.account_type, 
+  a.account_number 
+  FROM accounts a 
+  INNER JOIN customer_products cp ON a.customer_product_id = cp.id 
+  INNER JOIN customers c ON cp.customer_id = c.id 
+  INNER JOIN financial_products fp ON cp.product_id = fp.id`;
   try {
     const { rows } = await pool.query(query);
-    res.json(rows);
+    res.status(200).json(rows);
   } catch (error) {
     res.status(500).json({ msg: "Internal Server Error" });
     console.log(error);
+  }
+}
+
+// TODO: ver las transacciones de una cuenta
+export async function getAccountTransactions(req, res) {
+  const { id } = req.params;
+  const query = `SELECT 
+    t.transaction_type,
+    t.amount, 
+    t.description,
+    (SELECT account_number FROM accounts WHERE id = (t.details->>'destination_account_id')::uuid) AS destination_account_number,
+    t.created_at AS date 
+    FROM transactions t WHERE t.account_id = '${id}'`;
+  try {
+    const { rows } = await pool.query(query);
+    if (rows.length === 0) {
+      return res
+        .status(404)
+        .json({ error: "No transactions for this account" });
+    }
+
+    res.status(200).json(rows);
+  } catch (error) {
+    console.error("Error executing query", error);
+    res.status(500).json({ error: "Internal server error" });
   }
 }
--- backend/src/services/accounts/src/routes/accountRoutes.js
+++ backend/src/services/accounts/src/routes/accountRoutes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { read } from "../controllers/accountControllers.js";
+import {
+  getAccountTransactions,
+  read,
+} from "../controllers/accountControllers.js";
 import { verifyToken } from "../../../../../../shared/middlewares/auth.js";
 
 const router = Router();
 
 router.get("/", verifyToken, read);
+router.get("/search/transactions/:id", verifyToken, getAccountTransactions);
 
 export default router;
