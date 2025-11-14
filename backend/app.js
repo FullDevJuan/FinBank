50e2a43e7116d376a155c56f50996a2ad26ba130
FullDevJuan <juandev003@gmail.com>
2025-11-14 00:38:35 -0500

    cors sin origin

--- backend/app.js
+++ backend/app.js
@@ -11,9 +11,8 @@ const app = express();
 
 app.use(express.json());
 app.use(
-  cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
-  })
+  cors()
+  // {origin: process.env.FRONTEND_URL || "http://localhost:5173",}
 );
 
 app.use("/api/users", userService);
